c03a58e166e6ad1260f6e484251448a8b99f36a8
kapil11Agnihotri <kapil90444@gmail.com>
2023-06-29 15:06:08 +0530

    updated pieChart

--- src/Components/Chart/PieChart.js
+++ src/Components/Chart/PieChart.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import styles from "./PieChart.module.css";
 
 const PieChart = () => {
   const canvasRef = useRef(null);
@@ -47,55 +48,67 @@ const PieChart = () => {
   const drawChart = (chartData) => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    const colors = ["red", "green", "blue", "yellow", "orange", "purple"];
-    const chartRadius = Math.min(canvas.width, canvas.height) / 2;
+    const colors = ["purple", "green", "blue", "yellow", "brown", "red"];
+    const chartRadius = Math.min(canvas.width, canvas.height) / 4;
     const chartCenterX = canvas.width / 2;
     const chartCenterY = canvas.height / 2;
-
+  
     // Calculate the total sum of data values
     const total = chartData.data.reduce((sum, value) => sum + value, 0);
-
+  
     // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
-
+  
     // Draw pie slices
     let startAngle = 0;
     chartData.data.forEach((value, index) => {
-      const sliceAngle = (Math.PI * 2 * value) / total;
+      const sliceAngle = (2 * Math.PI * value) / total;
       const endAngle = startAngle + sliceAngle;
-
+  
       context.beginPath();
       context.fillStyle = colors[index % colors.length];
       context.moveTo(chartCenterX, chartCenterY);
-      context.arc(
-        chartCenterX,
-        chartCenterY,
-        chartRadius,
-        startAngle,
-        endAngle
-      );
+      context.arc(chartCenterX, chartCenterY, chartRadius, startAngle, endAngle);
       context.closePath();
       context.fill();
-
+  
+      startAngle = endAngle;
+    });
+  
+    // Draw labels and counts
+    let labelAngle = 0;
+    const labelRadius = chartRadius + 30;
+    chartData.labels.forEach((label, index) => {
+      const sliceAngle = (2 * Math.PI * chartData.data[index]) / total;
+      const midAngle = labelAngle + sliceAngle / 2; // Calculate the middle angle of the slice
+  
+      // Calculate the position of the label and count based on the middle angle
+      const labelX = chartCenterX + Math.cos(midAngle) * labelRadius;
+      const labelY = chartCenterY + Math.sin(midAngle) * labelRadius;
+      const countX = chartCenterX + Math.cos(midAngle) * (labelRadius + 15);
+      const countY = chartCenterY + Math.sin(midAngle) * (labelRadius + 15);
+  
       // Draw labels
-      const labelX =
-        chartCenterX +
-        Math.cos(startAngle + sliceAngle / 2) * (chartRadius / 2);
-      const labelY =
-        chartCenterY +
-        Math.sin(startAngle + sliceAngle / 2) * (chartRadius / 2);
       context.fillStyle = "black";
       context.font = "12px Arial";
       context.textAlign = "center";
-      context.fillText(`${chartData.labels[index]} (${value})`, labelX, labelY);
-
-      startAngle = endAngle;
+      context.fillText(label, labelX, labelY);
+  
+      // Draw counts
+      context.font = "bold 12px Arial";
+      context.fillText(chartData.data[index], countX, countY);
+  
+      labelAngle += sliceAngle;
     });
   };
+  
+  
+  
+  
 
   return (
-    <div>
-      <h3>Pie Chart</h3>
+    <div className={styles.pieChartContainer}>
+      <h3 className={styles.chartTitle}>Pie Chart</h3>
       <canvas ref={canvasRef} width={400} height={400} />
     </div>
   );
